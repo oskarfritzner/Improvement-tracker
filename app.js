832b9e08b465c8ac97a4fbf8089dfe96f5eaa73a
oskarfritzner <72510834+oskarfritzner@users.noreply.github.com>
2023-04-09 14:54:30 +0200

    added localdate functions and more

--- app.js
+++ app.js
@@ -1,43 +1,95 @@
-const addButtonList = document.querySelectorAll('.add-to-do-btn');
-const habits = [];
+const putUserNameOutput = document.getElementById("user-name-h2");
 
-/*
-Here's a brief explanation of what the code does:
+if (!localStorage.getItem("hasVisited")) {
+  let username = prompt("Velkommen til din personlige HabitTracker! Hva heter du?");
 
-We first get a list of all the "Add" buttons using document.querySelectorAll('.add-to-do-btn').
-We loop through this list using addButtonList.forEach().
-For each button, we add a click event listener using button.addEventListener('click', function(event) {...}).
-Inside the event listener, we first prevent the default form submission behavior using event.preventDefault().
-We then get the closest .p-container element using this.closest('.p-container'), where this refers to the clicked button.
-We get the input field value using participantContainer.querySelector('input[type="text"]').value.trim().
-If the input value is not empty, we create a new li element using document.createElement('li'), add the necessary classes and HTML using newLi.classList.add() and newLi.innerHTML, and append it to the appropriate to-do list using toDoList.appendChild(newLi).
-Finally, we clear the input field using inputField.value = ''.
+  localStorage.setItem("user", username);
+  putUserNameOutput.innerHTML = localStorage.getItem("user");
+  localStorage.setItem("hasVisited", true);
+} else {
+  putUserNameOutput.innerHTML = localStorage.getItem("user");
+}
 
-*/
+const addButtonList = document.querySelectorAll(".add-to-do-btn");
+const userhabitsSection = document.getElementById("user-habits-section");
 
-addButtonList.forEach(function(button) {
-  button.addEventListener('click', function(event) {
+const storedHabits = JSON.parse(localStorage.getItem("habits")) || [];
+const habits = storedHabits;
+
+function showStoredHabits(habits) {
+  habits.forEach((habitObj) => {
+    const newLi = document.createElement("li");
+    newLi.classList.add("checkbox-container");
+    newLi.innerHTML = `
+        <label for="${habitObj.habit}-button">${habitObj.habit}</label>
+        <button class="check-habit ${habitObj.finishedHabit ? 'completed' : ''}" id="${habitObj.habit}-button" name="myCheckbox" value="${habitObj.habit}"></button>
+        <button class="remove-habit">X</button>
+      `;
+    userhabitsSection.appendChild(newLi);
+  });
+}
+
+
+showStoredHabits(habits);
+
+addButtonList.forEach(function (button) {
+  button.addEventListener("click", function (event) {
     event.preventDefault();
-    const participantContainer = this.closest('.p-container');
+    const participantContainer = this.closest(".p-container");
     const inputField = participantContainer.querySelector('input[type="text"]');
     const inputValue = inputField.value.trim();
 
-    console.log(inputValue);
-
-    if (inputValue !== '') {
-      habits.push(inputValue);
+    if (inputValue !== "") {
+      habits.push({habit:inputValue, finishedHabit: false});
       localStorage.setItem("habits", JSON.stringify(habits));
-      
-      const toDoList = participantContainer.querySelector('.to-do');
-      const newLi = document.createElement('li');
-      newLi.classList.add('checkbox-container');
+  
+
+      const toDoList = participantContainer.querySelector(".to-do");
+      const newLi = document.createElement("li");
+      newLi.classList.add("checkbox-container");
       newLi.innerHTML = `
-        <label for="${this.id}-checkbox">${inputValue}</label>
-        <input type="checkbox" id="${this.id}-checkbox" name="myCheckbox" value="${inputValue}">
+        <label for="${inputValue}-button">${inputValue}</label>
+        <button class="check-habit" id="${inputValue}-button" name="myCheckbox" value="${inputValue}"></button>
+        <button class="remove-habit">X</button>
       `;
       toDoList.appendChild(newLi);
+      newLi.querySelector(".remove-habit").addEventListener("click", removeHabit);
 
-      inputField.value = '';
+      inputField.value = "";
     }
   });
+});
+
+function removeHabit(event) {
+  const habitLi = event.target.closest("li");
+  const habitButton = habitLi.querySelector("button.check-habit");
+  const habitValue = habitButton.value;
+  const habitIndex = habits.findIndex(habitObj => habitObj.habit === habitValue);
+
+  if (habitIndex > -1) {
+    habits.splice(habitIndex, 1);
+    localStorage.setItem("habits", JSON.stringify(habits));
+  }
+  habitLi.remove();
+}
+
+
+userhabitsSection.addEventListener("click", function (event) {
+  if (event.target.classList.contains("remove-habit")) {
+    removeHabit(event);
+  }
+});
+
+userhabitsSection.addEventListener("click", function (event) {
+  if (event.target.classList.contains("check-habit")) {
+    event.target.classList.toggle("completed");
+    
+    const habitValue = event.target.value;
+    const habitIndex = habits.findIndex(habitObj => habitObj.habit === habitValue);
+
+    if (habitIndex > -1) {
+      habits[habitIndex].finishedHabit = !habits[habitIndex].finishedHabit;
+      localStorage.setItem("habits", JSON.stringify(habits));
+    }
+  }
 });
